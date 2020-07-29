ae4d7f131bf10a210400fa77b66b4fe285ee889f
BinThani <62651802+BinThani@users.noreply.github.com>
2020-07-29 14:56:01 +0400

    Create ps0.py

---
+++ ps0.py
@@ -0,0 +1,8 @@
+#PSET 0 100%
+import numpy
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+exponent = str(x**y)
+print("x**y = " + exponent)
+print(f"log(x) = {numpy.log2(x)}")
